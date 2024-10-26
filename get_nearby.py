114b693c96fecb2af07b60cfbc5949e0f33d78b9
jbedette <john.bedette45@gmail.com>
2024-10-26 14:47:58 -0700

    added more keywords, fixed a bug when location not found

--- get_nearby.py
+++ get_nearby.py
@@ -3,6 +3,12 @@ from geopy.distance import geodesic
 from dotenv import load_dotenv
 import os
 
+#debug
+import inspect
+import tools
+debug_true = False
+#debug
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -12,6 +18,10 @@ MY_ADDR = os.getenv('MY_ADDR')
 
 # Function to get latitude and longitude of a given address
 def get_coordinates(address):
+    # debug
+    # print(f"{inspect.currentframe().f_code.co_name}")
+    tools.print_debug(inspect.currentframe().f_code.co_name,debug_true)
+    # debug
     try:
         geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
         response = requests.get(geocode_url)
@@ -30,6 +40,10 @@ def get_coordinates(address):
 
 # Function to get nearby restaurants
 def get_nearby(lat, lng, radius, keyword):
+    # debug
+    # print(f"{inspect.currentframe().f_code.co_name}")
+    tools.print_debug(inspect.currentframe().f_code.co_name,debug_true)
+    # debug
     try:
         places_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius={radius}&type={keyword}&key={API_KEY}"
         response = requests.get(places_url)
@@ -39,7 +53,7 @@ def get_nearby(lat, lng, radius, keyword):
         if result['status'] == 'OK' and result['results']:
             return result['results']
         else:
-            print(f"Places API error: {result['status']}")
+            # print(f"Places API error: {result['status']}")
             return []
     except requests.exceptions.RequestException as e:
         print(f"Error fetching nearby {keyword}: {e}")
@@ -47,9 +61,17 @@ def get_nearby(lat, lng, radius, keyword):
 
 # Function to calculate the distance between two points
 def calculate_distance(loc1, loc2):
+    # debug
+    # print(f"{inspect.currentframe().f_code.co_name}")
+    tools.print_debug(inspect.currentframe().f_code.co_name,debug_true)
+    # debug
     return geodesic(loc1, loc2).kilometers
 
 def get_closest(origin_coord, keyword, places, num_results):
+    # debug
+    # print(f"{inspect.currentframe().f_code.co_name}")
+    tools.print_debug(inspect.currentframe().f_code.co_name,debug_true)
+    # debug
     # Store the restaurant and distance
     lat, lng = origin_coord
     distances = []
@@ -80,6 +102,10 @@ def get_closest(origin_coord, keyword, places, num_results):
 
 # Main function to get closest restaurants
 def get_nearby_poi(address, keyword, radius_meters, num_results):
+    # debug
+    # print(f"{inspect.currentframe().f_code.co_name}")
+    tools.print_debug(inspect.currentframe().f_code.co_name,debug_true)
+    # debug
     coordinates = get_coordinates(address)
     if not coordinates:
         print("Address not found.")
@@ -89,25 +115,23 @@ def get_nearby_poi(address, keyword, radius_meters, num_results):
     places = get_nearby(lat, lng, radius_meters, keyword )
 
     if not places:
-        print("No {keyword} found nearby.")
-        return
+        # print(f"No {keyword} found nearby.")
+        return [],[]
 
     closest = get_closest(coordinates, keyword, places, num_results )
 
     return places, closest
 
+def print_items(items):
+    for item in items:
+        print('\n',item)
+        for i, c in enumerate(items, 1):
+            print(f"{i}. {c['name']}")
+            print(f"   address: {c['address']}")
+            print(f"   distance: {c['distance_km']:.2f} km\n")
 
-if __name__ == "__main__":
-    # user_address = input("Enter your address: ")
-    # keyword = input("Enter the type of landmark you are looking for: ")
+def get_keyword_places(keywords, radius_meters, num_results):
     results = []
-
-    keywords = [
-                "restaurant",
-                "park"
-                ]
-    radius_meters = 1000
-    num_results = 5
     # keyword = "restaurant"
     for keyword in keywords:
         places, closest = get_nearby_poi(MY_ADDR, keyword, radius_meters, num_results)
@@ -117,13 +141,125 @@ if __name__ == "__main__":
             'places': places,
             'close': closest
         })
+    for result in results:
+        print("\n=========\n", keyword, '\n')
+        for i, addr in enumerate(result['close'],1):
+            print(f"{i}. {addr['name']}")
+            print(f"    addr: {addr['address']}")
+            print(f"    dist: {addr['distance_km']:.2f} km\n")
+    return results
     
     
 
+
+if __name__ == "__main__":
+    # user_address = input("Enter your address: ")
+    # keyword = input("Enter the type of landmark you are looking for: ")
+
+    # keywords = [
+    #             "bowling_alley",
+    #             "restaurant",
+    #             "park"
+    #             ]
+                
+    keywords = [
+        "art_gallery",
+        "atm",
+        "bakery",
+        "bank",
+        "bar",
+        "book_store",
+        "bowling_alley",
+        "bus_station",
+        "cafe",
+        "clothing_store",
+        "convenience_store",
+        "department_store",
+        "drugstore",
+        "florist",
+        "funeral_home",
+        "furniture_store",
+        "gym",
+        "hardware_store",
+        "home_goods_store",
+        "jewelry_store",
+        "library",
+        "light_rail_station",
+        "liquor_store",
+        "movie_theater",
+        "museum",
+        "night_club",
+        "park",
+        "pet_store",
+        "pharmacy",
+        "primary_school",
+        "restaurant",
+        "school",
+        "spa",
+        "supermarket",
+        "train_station",
+        "transit_station",
+        "university"
+    ]
+
+    radius_meters = 1000
+    num_results = 5
+    # get_keyword_places(good_keywords,radius_meters,num_results)
+    results = []
+    not_found = []
+    found_score = 0
+    # keyword = "restaurant"
+    for keyword in keywords:
+        #debug
+        tools.print_debug("=====>" + str(keyword),debug_true)
+        #debug
+
+        places, closest = get_nearby_poi(MY_ADDR, keyword, radius_meters, num_results)
+
+        if not places:
+            # results.append({
+            #     'keyword': keyword,
+            #     'close': [],
+            #     'found': False
+            # })
+            not_found.append(keyword)
+        else:
+            results.append({
+                'keyword': keyword,
+                'places': places,
+                'close': closest,
+                'found': True
+            })
+            found_score += 1
+    
+    
+        # printing data captured from api
+        # 
     #     print('\n',keyword)
-    #     print(closest)
-        # for i, c in enumerate(closest, 1):
-        #     print(f"{i}. {c['name']}")
-        #     print(f"   address: {c['address']}")
-        #     print(f"   distance: {c['distance_km']:.2f} km\n")
+    #     # print(closest)
+    #     for i, c in enumerate(closest, 1):
+    #         print(f"{i}. {c['name']}")
+    #         print(f"   address: {c['address']}")
+    #         print(f"   distance: {c['distance_km']:.2f} km\n")
+
+    # printing stored found results
+    for result in results:
+        print("\n=========\n", result['keyword'], '\n')
+        for i, addr in enumerate(result['close'],1):
+                print(f"{i}. {addr['name']}")
+                print(f"    addr: {addr['address']}")
+                print(f"    dist: {addr['distance_km']:.2f} km\n")
+
+    print(f"==========\found:")
+    for i,r in enumerate(results,start=1):
+        print(f"{i}. {r['keyword']}")
+    print(f"==========\nFound Score: {found_score}\n")
+
+
+    print(f"============\nnot found:")
+    for i,result in enumerate(not_found,start=1):
+        print(f"{i}. {result}")
+    print(f"")
+
+
             
---
+++ tools.py
@@ -0,0 +1,3 @@
+def print_debug(name, debug_true):
+    if(debug_true):
+        print(f"{name}")
